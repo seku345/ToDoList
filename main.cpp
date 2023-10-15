d1b8dc416396e33d9be5e3e93712515f580ededa
seku <seku345@yandex.ru>
2023-10-15 17:18:03 +0300

    saving and adding

--- main.cpp
+++ main.cpp
@@ -35,11 +35,11 @@ void load_db(std::string path, std::vector<std::vector<std::string>>& tasks)
         tasks.push_back(separated_line);
     }
     file.close();
-    
 }
 
 void print_db(std::vector<std::vector<std::string>> tasks)
 {
+    std::cout << "Here are your tasks:\n";
     for (int i = 0; i < tasks.size(); ++i)
     {
         std::cout << i+1 << ") ";
@@ -49,11 +49,38 @@ void print_db(std::vector<std::vector<std::string>> tasks)
         }
         std::cout << '\n';
     }
+    std::cout << '\n';
+}
+
+void add_task(std::vector<std::vector<std::string>>& tasks)
+{
+    std::string name, description, string_date, string_time, status = "✘";
+    std::cout << "Write the name of your task: ";
+    std::cin >> name;
+    std::cout << "Write the description of your task:\n\t";
+    std::cin >> description;
+    std::cout << "Write the deadline date or 0 if there is no deadline: ";
+    std::cin >> string_date;
+    std::cout << "Write the time by which you need to do the task\nor 0 if the task can be done at any time: ";
+    std::cin >> string_time;
+    std::vector<std::string> task{name, description, string_date, string_time, status};
+    tasks.push_back(task);
 }
 
 void save_db(std::string path, std::vector<std::vector<std::string>> tasks)
 {
-
+    std::fstream file(path);
+    for (std::vector<std::string> line : tasks)
+    {
+        std::string export_line = "";
+        for (std::string part : line)
+        {
+            export_line += part + "\t";
+        }
+        export_line += '\n';
+        file << export_line;
+    }
+    file.close();
 }
 
 int main()
@@ -65,6 +92,7 @@ int main()
     bool working = true;
     while (working)
     {
+        print_db(tasks);
         std::cout << "Choose what you want to do:\n\t1. Add task\n\t2. Edit task\n\t3. View tasks\nOr print 0 to exit.\n";
         char mode;
         std::cin >> mode;
@@ -75,13 +103,12 @@ int main()
                 save_db("tasks.txt", tasks);
                 break;
             case '1':
-                
+                add_task(tasks);
                 break;
             case '2':
 
                 break;
             case '3':
-                print_db(tasks);
                 break;
             default:
                 std::cout << "Invalid input! Try again.\n";
