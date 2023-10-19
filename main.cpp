fc2f2bd8411b7b9767f942e6dbf24dc3771a00dc
seku <seku345@yandex.ru>
2023-10-19 18:11:14 +0300

    it works

--- main.cpp
+++ main.cpp
@@ -38,23 +38,23 @@ void load_db(std::string path, std::vector<std::vector<std::string>>& tasks)
     file.close();
 }
 
-void save_db(std::string path, std::vector<std::vector<std::string>>& tasks)
+void save_db(std::string path, std::vector<std::vector<std::string>> tasks)
 {
-    std::fstream file(path);
+    std::fstream file(path, std::ios_base::out | std::ios_base::trunc);
+    std::string export_line = "";
     for (std::vector<std::string> line : tasks)
     {
-        std::string export_line = "";
         for (std::string part : line)
         {
             export_line += part + "\t";
         }
         export_line += '\n';
-        file << export_line;
     }
+    file << export_line;
     file.close();
 }
 
-void print_db(std::vector<std::vector<std::string>>& tasks)
+void print_db(std::vector<std::vector<std::string>> tasks)
 {
     std::cout << "Here are your tasks:\n";
     for (int i = 0; i < tasks.size(); ++i)
@@ -88,23 +88,54 @@ void add_task(std::vector<std::vector<std::string>>& tasks)
 
 void edit_task(std::vector<std::vector<std::string>>& tasks)
 {
-
+    std::cout << "Write the number of the task you want to edit: ";
+    int task_num = 0; //TODO VALIDATION
+    std::cin >> task_num;
+    std::cout << "Select what you want to edit:\n\t1. Name\n\t2. Description\n\t3. Date deadline\n\t4. Time deadline\n";
+    std::string choice;
+    std::cin >> choice;
+    if (choice == "1")
+    {
+        std::cout << "Enter a new name for the " << task_num << " task: ";
+        char name[64];
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.getline(name, 64);
+        tasks[task_num-1][0] = name;
+    }
+    else if (choice == "2")
+    {
+        std::cout << "Enter a new description for the " << task_num << " task:\n\t";
+        char description[256];
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.getline(description, 256);
+        tasks[task_num-1][1] = description;
+    }
+    else if (choice == "3")
+    {
+        std::cout << "Enter a new date deadline for the " << task_num << " task: ";
+        std::string date;
+        std::cin >> date;
+        tasks[task_num-1][2] = date;
+    }
+    else if (choice == "4")
+    {
+        std::cout << "Enter a new time deadline for the " << task_num << " task: ";
+        std::string time;
+        std::cin >> time;
+        tasks[task_num-1][3] = time;
+    }
+    else
+    {
+        std::cout << "Invalid input!";
+    }
 }
 
-std::vector<std::vector<std::string>> deleted_tasks(std::vector<std::vector<std::string>> tasks)
+void delete_task(std::vector<std::vector<std::string>>& tasks)
 {
     int task_num = 0;
     std::cout << "Write the number of the task you want to delete: ";
     std::cin >> task_num;
-    std::vector<std::vector<std::string>> new_tasks;
-    for (int i = 0; i < tasks.size(); ++i)
-    {
-        if (!(i == task_num-1))
-        {
-            new_tasks.push_back(tasks[i]);
-        }
-    }
-    return new_tasks;
+    tasks.erase(tasks.begin() + task_num - 1, tasks.begin() + task_num);
 }
 
 void switch_status(std::vector<std::vector<std::string>>& tasks)
@@ -133,43 +164,55 @@ int main()
     std::vector<std::vector<std::string>> tasks;
     load_db("tasks.txt", tasks);
     
-    //TODO switch to if-else!
     bool working = true;
     while (working)
     {
+        // main menu
         print_db(tasks);
         std::cout << "Choose what you want to do:\n\t1. Add the task\n\t2. Edit task\n\t3. Switch the status of the task\n\t4. Delete task\n\t5. Sort tasks\nOr print 0 to exit.\n";
-        char mode;
+        std::string mode;
         std::cin >> mode;
-        switch (mode)
+        // exit & saving
+        if (mode == "0")
         {
-            case '0':
-                working = false;
-                save_db("tasks.txt", tasks);
-                break;
-            case '1':
-                add_task(tasks);
-                save_db("tasks.txt", tasks);
-                break;
-            case '2':
-                edit_task(tasks);
-                save_db("tasks.txt", tasks);
-                break;
-            case '3':
-                switch_status(tasks);
-                save_db("tasks.txt", tasks);
-                break;
-            case '4':
-                tasks = deleted_tasks(tasks);
-                save_db("tasks.txt", tasks);
-                break;
-            case '5':
-                sort_tasks(tasks);
-                save_db("tasks.txt", tasks);
-                break;
-            default:
-                std::cout << "Invalid input! Try again.\n";
-                break;
+            working = false;
+            save_db("tasks.txt", tasks);
+            break;
+        }
+        // adding task
+        else if (mode == "1")
+        {
+            add_task(tasks);
+            save_db("tasks.txt", tasks);
+        }
+        // editing task
+        else if (mode == "2")
+        {
+            edit_task(tasks);
+            save_db("tasks.txt", tasks);
+        }
+        // completing task
+        else if (mode == "3")
+        {
+            switch_status(tasks);
+            save_db("tasks.txt", tasks);
+        }
+        // deleting task
+        else if (mode == "4")
+        {
+            delete_task(tasks);
+            save_db("tasks.txt", tasks);
+        }
+        // sorting tasks
+        else if (mode == "5")
+        {
+            sort_tasks(tasks);
+            save_db("tasks.txt", tasks);
+        }
+        // validation
+        else
+        {
+            std::cout << "Invalid input! Try again.\n";
         }
     }
 }
